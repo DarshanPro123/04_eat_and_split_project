9afbea36802095fe16f98fc18d369e15490a513a
Darshanpro123 <darshanpro187@gmail.com>
2024-09-30 15:57:43 +0530

    add edited file of list cards of friends

--- src/Components/FriendsList.jsx
+++ src/Components/FriendsList.jsx
@@ -6,40 +6,37 @@ const FriendsList = () => {
     <div className="lists-box">
       <h2>My Friend List</h2>
       <div className="lists">
-        <ul>
-          {friends.map((friend) => {
-            return (
-              <>
-                <li key={friend.id}>
-                  <div className="content">
-                    {" "}
-                    <img src={friend.image} alt="" />
-                    <div className="frnd-info">
-                      <h3>{friend.name}</h3>
-                      {friend.balance < 0 && (
-                        <p className="red">
-                          You debt with {friend.name} is $
-                          {Math.abs(friend.balance)}
-                        </p>
-                      )}
-                      {friend.balance > 0 && (
-                        <p className="green">
-                          {friend.name} debt with you is $
-                          {Math.abs(friend.balance)}
-                        </p>
-                      )}
-                      {friend.balance === 0 && (
-                        <p>You and {friend.name} all clear</p>
-                      )}
-                    </div>
-                  </div>
-                  <CommonBtn>Select</CommonBtn>
-                </li>
-              </>
-            );
-          })}
-        </ul>
+        {friends.map((friend) => {
+          return (
+            <>
+              <div className="list" key={friend.id}>
+                <img src={friend.image} alt="" />
+
+                <div className="main-content">
+                  <h3>{friend.name}</h3>
+                  {friend.balance < 0 && (
+                    <p className="red">
+                      You debt with {friend.name} of ${" "}
+                      {Math.abs(friend.balance)}
+                    </p>
+                  )}
+                  {friend.balance > 0 && (
+                    <p className="green">
+                      {friend.name} debt with you of $ {friend.balance}
+                    </p>
+                  )}
+                  {friend.balance === 0 && (
+                    <p className="blue">You debt with {friend.name} clear </p>
+                  )}
+                </div>
+
+                <CommonBtn>Select</CommonBtn>
+              </div>
+            </>
+          );
+        })}
       </div>
+      <CommonBtn>Add List</CommonBtn>
     </div>
   );
 };
