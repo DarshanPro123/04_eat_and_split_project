eb418a6f3294cfcda7bd856131c1317ee15cd19d
Darshanpro123 <darshanpro187@gmail.com>
2024-10-01 16:13:23 +0530

    edit css bgimg on form and data.js

--- public/data.js
+++ public/data.js
@@ -17,18 +17,6 @@ const initialFriends = [
     image: "https://i.pravatar.cc/23?u=118836",
     balance: 0,
   },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/78?u=118826",
-    balance: 0,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/32?u=112336",
-    balance: 0,
-  },
 ];
 
 export default initialFriends;
